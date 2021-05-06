2d242154e937534621782e67f5d7acdfbc6e3de8
William Rupert <wabokeglobal@gmain.com>
2021-05-06 01:34:51 +0100

    commit

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import customer, products, Order
 
 # Register your models here.
+admin.site.register(customer)
+admin.site.register(products)
+admin.site.register(Order)
--- main/models.py
+++ main/models.py
@@ -1,10 +1,34 @@
 from django.db import models
 
 # Create your models here.
-class Tutorial(models.Model):
-    tutorial_title = models.CharField(max_length=200)
-    tutorial_cntent = models.TextField()
-    tutorial_published = models.DateTimeField("Date Published")
+class products(models.Model):
+    CATEGORY =(
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door')
+    )
+    product_name = models.CharField(max_length=200, null=True)
+    product_category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    product_price = models.FloatField(null=True)
+    product_description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField("Date Created", auto_now_add =True, null=True)
     def __str__(self):
-        return self.tutorial_title
+        return self.product_name
     
+class customer(models.Model):
+    customer_name = models.CharField(max_length=200, null=True)
+    customer_phone = models.CharField(max_length=200, null=True)
+    customer_email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField("Date Created", auto_now_add =True, null=True)
+    def __str__(self):
+        return self.customer_name
+
+class Order(models.Model):
+    STATUS =(
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivery', 'Delivery'),
+    )
+    #customer =
+    #product =
+    date_created = models.DateTimeField("Date Created", auto_now_add =True, null=True)
+    status = models.CharField(max_length=200, null=True, choices = STATUS)
